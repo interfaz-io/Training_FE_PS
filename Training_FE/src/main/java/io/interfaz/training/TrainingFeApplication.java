e746b10a552b225810d3f39a97cb352ed550fa8b
Jose Zamora <jzamoras@interfaz.io>
2021-08-20 13:56:57 -0600

    Creacion de Proyecto

---
+++ Training_FE/src/main/java/io/interfaz/training/TrainingFeApplication.java
@@ -0,0 +1,14 @@
+package io.interfaz.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingFeApplication.class, args);
+		
+	}
+
+}
---
+++ Training_FE/src/test/java/io/interfaz/training/TrainingFeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.interfaz.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingFeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
