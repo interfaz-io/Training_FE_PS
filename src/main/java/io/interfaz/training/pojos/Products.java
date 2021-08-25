e255f7f6afc203d9f0819ef2cdfabbafd8294440
Jose Zamora <jzamoras@interfaz.io>
2021-08-25 13:17:48 -0600

    Actualizar product

--- src/main/java/io/interfaz/training/pojos/Products.java
+++ src/main/java/io/interfaz/training/pojos/Products.java
@@ -3,71 +3,83 @@
  */
 package io.interfaz.training.pojos;
 
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 /**
  * @author María
  *
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Products {
-	private int id;
+	
+	@NonNull 
+	private Integer id;
+	
 	private String name;
+	
 	private String description;
-	private double price;
+	
+	private BigDecimal price;
+	
 	private String status;
 	
-	public Products(int id, String name, String description, double price, String status) {
-		this.id = id;
-		this.name = name;
-		this.description = description;
-		this.price = price;
-		this.status = status;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
+//	public Products(int id, String name, String description, double price, String status) {
+//		this.id = id;
+//		this.name = name;
+//		this.description = description;
+//		this.price = price;
+//		this.status = status;
+//	}
+//
+//	public int getId() {
+//		return id;
+//	}
+//
+//	public void setId(int id) {
+//		this.id = id;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getDescription() {
+//		return description;
+//	}
+//
+//	public void setDescription(String description) {
+//		this.description = description;
+//	}
+//
+//	public double getPrice() {
+//		return price;
+//	}
+//
+//	public void setPrice(double price) {
+//		this.price = price;
+//	}
+//
+//	public String getStatus() {
+//		return status;
+//	}
+//
+//	public void setStatus(String status) {
+//		this.status = status;
+//	}
 	
 	
 
