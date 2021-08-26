83d96398209fa267b5b6f7d066d3ae49d8d69f6c
Maria <mrodriguez@interfaz.io>
2021-08-25 18:16:17 -0600

    serach by email

--- src/main/java/io/interfaz/training/controllers/OrderController.java
+++ src/main/java/io/interfaz/training/controllers/OrderController.java
@@ -32,37 +32,42 @@ public class OrderController {
 
 	@Autowired
 	private OrderService serviceOrder;
-	
-	@Autowired 
+
+	@Autowired
 	private CustomerService serviceCustomer;
-	
+
 	@Autowired
 	private ProductService serviceProduct;
 
-	//<td><a th:href="'/people/getById/' + ${person.id}" th:text="${person.id}"></a></td>
 
 	@GetMapping()
 	public String getAllOrders(Model model) {
-		model.addAttribute("customer2", "");
+		model.addAttribute("customer2", "-----");
 		model.addAttribute("orders", serviceOrder.getAllOrder());
 		model.addAttribute("customers", serviceCustomer.getAllCustomer());
 		return "web/order/orderAdmin";
 	}
-	
-	//el model lo puse para poder pasar la lista de productos quemada
+
 	@GetMapping("/add")
-	public String addOrder(Model model){
+	public String addOrder(Model model) {
 		model.addAttribute("products", serviceProduct.getAllProduct());
 		return "web/order/addOrder";
 	}
-	
+
 	@GetMapping("/getCustomer")
 	public String getCustomer(String customers, Model model) {
-		
-		model.addAttribute("orders", serviceOrder.getAllOrder());
+		System.out.println( customers);
+		if(customers==null) {
+			model.addAttribute("customer2", "hola");
+			
+		}else {
+			
+			model.addAttribute("customer2", serviceCustomer.getCustomerByEmail(customers).getEmail());
+		}
+		model.addAttribute("orders", serviceOrder.getOrderByCustomer(serviceCustomer.getCustomerByEmail(customers).getId()));
 		model.addAttribute("customers", serviceCustomer.getAllCustomer());
 		model.addAttribute("customer2", customers);
 		return "web/order/orderAdmin";
 	}
-	
-}
+	}
+
