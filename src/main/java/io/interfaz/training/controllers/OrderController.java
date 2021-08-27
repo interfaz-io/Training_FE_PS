7c99fb15636a9b90f55f757276f51655791e42fc
Maria <mrodriguez@interfaz.io>
2021-08-26 18:54:39 -0600

    Arreglos en orders

--- src/main/java/io/interfaz/training/controllers/OrderController.java
+++ src/main/java/io/interfaz/training/controllers/OrderController.java
@@ -6,6 +6,8 @@ package io.interfaz.training.controllers;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import io.interfaz.training.pojos.Customers;
 import io.interfaz.training.pojos.Orders;
@@ -29,7 +32,7 @@ import io.interfaz.training.services.ProductService;
  * @author Nacho
  *
  */
-@SessionAttributes({"customer2", "orderNew"})
+@SessionAttributes({ "customer2", "orderNew" })
 @RequestMapping("/order")
 @Controller
 public class OrderController {
@@ -46,14 +49,29 @@ public class OrderController {
 	@Autowired
 	private OrderDetailsService serviceOrderDetail;
 
+	@ModelAttribute("customer2")
+	public Customers customer2() {
+		return new Customers(null, "", "", null, null, null, null);
+	}
 	@GetMapping()
-	public String getAllOrders(Model model) {
-//		model.addAttribute("customer2", new Customers(0, "prueba", "prueba", "prueba", 0, "prueba", null));
-	//	model.addAttribute("orders", serviceOrder.getAllOrder());
-	//	model.addAttribute("customers", serviceCustomer.getAllCustomer());
+	public String getAllOrders(Model model, @ModelAttribute("customer2") Customers customer2) {
+		
+		if ((null != customer2.getEmail()) && (!customer2.getEmail().isBlank())) {
+			model.addAttribute("orders",serviceOrder.getOrderByCustomer(serviceCustomer.getCustomerByEmail(customer2.getEmail()).getId()));
+		}
+
 		return "web/order/orderAdmin";
 	}
 
+//	@GetMapping("/all")
+//	public String all (Model model,@ModelAttribute("customer2") Customers customer2, HttpSession session){
+//		if (!customer2.getEmail().equalsIgnoreCase("")) {
+//			model.addAttribute("orders",serviceOrder.getOrderByCustomer(serviceCustomer.getCustomerByEmail(customer2.getEmail()).getId()));
+//		}
+//		session.setAttribute("name", "Anthony");
+//		return "web/order/orderAdmin";
+//	}
+	
 	/*
 	 * @GetMapping("/add") public String addOrder(Model model, String keyword){
 	 * if(keyword != null && !keyword.equalsIgnoreCase("")) {
@@ -77,9 +95,10 @@ public class OrderController {
 	 * 
 	 * return "web/order/addOrder"; }
 	 */
-	
+
 	@GetMapping("/add")
-	public String addOrder(Model model, String keyword, @ModelAttribute("orderNew") Orders orderNew, @ModelAttribute("customer2") Customers customer2) {
+	public String addOrder(Model model, String keyword, @ModelAttribute("orderNew") Orders orderNew,
+			@ModelAttribute("customer2") Customers customer2) {
 		System.out.println("order id para meter: " + orderNew.getId());
 		// System.out.println("customer "+ customer2.getFirstName());
 		if (keyword != null && !keyword.equalsIgnoreCase("")) {
@@ -100,22 +119,21 @@ public class OrderController {
 		// getAllProducts(model);
 		return "web/order/addOrder";
 	}
-	
-	
+
 	@PostMapping("/addNewOrder")
 	public String addNewOrder(Model model, @ModelAttribute("customer2") Customers customer2) {
-		Orders newOrder = serviceOrder.addOrder(new Orders(0, new Date(), customer2.getId(),
-				BigDecimal.valueOf(0), BigDecimal.valueOf(0), BigDecimal.valueOf(0)));
+		Orders newOrder = serviceOrder.addOrder(new Orders(0, new Date(), customer2.getId(), BigDecimal.valueOf(0),
+				BigDecimal.valueOf(0), BigDecimal.valueOf(0)));
 		model.addAttribute("orderNew", newOrder);
 		System.out.println(newOrder);
 		return "web/order/addOrder";
 	}
 
 	@PostMapping("/addOrderDetail")
-	public String addOrderDetails(Integer idProduct,  @ModelAttribute("orderNew") Orders orderNew, Model model) {
-		 System.out.println("id product: " + idProduct);
-		OrdersDetails newOrderDetail = serviceOrderDetail.addOrderDetails(new OrdersDetails(0, orderNew.getId(), idProduct, 0,
-				BigDecimal.valueOf(0), 0, productOrder.getProduct(idProduct)));
+	public String addOrderDetails(Integer idProduct, @ModelAttribute("orderNew") Orders orderNew, Model model) {
+		System.out.println("id product: " + idProduct);
+		OrdersDetails newOrderDetail = serviceOrderDetail.addOrderDetails(new OrdersDetails(0, orderNew.getId(),
+				idProduct, 0, BigDecimal.valueOf(0), 0, productOrder.getProduct(idProduct)));
 		// model.addAttribute("orderNew", idOrder);
 		return "web/order/addOrder";
 	}
@@ -128,17 +146,15 @@ public class OrderController {
 
 	@GetMapping("/getCustomer")
 	public String getCustomer(String email, Model model) {
-//		if (email.isEmpty() ) {
-//			model.addAttribute("customer2", new Customers(12, "", "", "", 0, "", null));
-//		} else {
-//		
-				model.addAttribute("customer2", serviceCustomer.getCustomerByEmail(email));
-//			
-//			
-//		}
+		
+		model.addAttribute("customer2", serviceCustomer.getCustomerByEmail(email));
 		model.addAttribute("orders",serviceOrder.getOrderByCustomer(serviceCustomer.getCustomerByEmail(email).getId()));
-	//	model.addAttribute("customers", serviceCustomer.getAllCustomer());
+
 		return "web/order/orderAdmin";
 	}
-
+	@GetMapping("/return")
+	public String status(SessionStatus status) {
+		status.setComplete();
+		return "web/home";
+	}
 }
