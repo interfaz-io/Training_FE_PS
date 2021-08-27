a936ea455ddee5a7fab0c804c319c2e5b71ffeea
Maria <mrodriguez@interfaz.io>
2021-08-27 09:00:04 -0600

    Cambios en orders y ordersDetails

--- src/main/java/io/interfaz/training/controllers/OrderController.java
+++ src/main/java/io/interfaz/training/controllers/OrderController.java
@@ -55,68 +55,22 @@ public class OrderController {
 	}
 	@GetMapping()
 	public String getAllOrders(Model model, @ModelAttribute("customer2") Customers customer2) {
-		
 		if ((null != customer2.getEmail()) && (!customer2.getEmail().isBlank())) {
 			model.addAttribute("orders",serviceOrder.getOrderByCustomer(serviceCustomer.getCustomerByEmail(customer2.getEmail()).getId()));
 		}
-
 		return "web/order/orderAdmin";
 	}
-
-//	@GetMapping("/all")
-//	public String all (Model model,@ModelAttribute("customer2") Customers customer2, HttpSession session){
-//		if (!customer2.getEmail().equalsIgnoreCase("")) {
-//			model.addAttribute("orders",serviceOrder.getOrderByCustomer(serviceCustomer.getCustomerByEmail(customer2.getEmail()).getId()));
-//		}
-//		session.setAttribute("name", "Anthony");
-//		return "web/order/orderAdmin";
-//	}
-	
-	/*
-	 * @GetMapping("/add") public String addOrder(Model model, String keyword){
-	 * if(keyword != null && !keyword.equalsIgnoreCase("")) {
-	 * model.addAttribute("products", productOrder.searchProductByName(keyword));
-	 * }else { model.addAttribute("products", productOrder.getAllProduct()); }
-	 * 
-	 * //getAllProducts(model); return "web/order/addOrder"; }
-	 */
-
-	/*
-	 * @GetMapping("/addNewOrder") public String addNewOrder(Model
-	 * model, @ModelAttribute("customer2") Customers customer2, String keyword) {
-	 * 
-	 * Orders newOrder = serviceOrder.addOrder(new Orders(null, new Date(),
-	 * customer2.getId(), BigDecimal.valueOf(0), BigDecimal.valueOf(0),
-	 * BigDecimal.valueOf(0))); model.addAttribute("orderNew", newOrder.getId());
-	 * 
-	 * if (keyword != null && !keyword.equalsIgnoreCase("")) {
-	 * model.addAttribute("products", productOrder.searchProductByName(keyword)); }
-	 * else { model.addAttribute("products", productOrder.getAllProduct()); }
-	 * 
-	 * return "web/order/addOrder"; }
-	 */
 
 	@GetMapping("/add")
 	public String addOrder(Model model, String keyword, @ModelAttribute("orderNew") Orders orderNew,
 			@ModelAttribute("customer2") Customers customer2) {
-		System.out.println("order id para meter: " + orderNew.getId());
-		// System.out.println("customer "+ customer2.getFirstName());
+
 		if (keyword != null && !keyword.equalsIgnoreCase("")) {
 			model.addAttribute("products", productOrder.searchProductByName(keyword));
 		} else {
 			model.addAttribute("products", productOrder.getAllProduct());
 		}
-//
-//		if (orderNew.getId() == null) {
-//			Orders newOrder = serviceOrder.addOrder(new Orders(null, new Date(), customer2.getId(),
-//					BigDecimal.valueOf(0), BigDecimal.valueOf(0), BigDecimal.valueOf(0)));
-//			model.addAttribute("orderNew", newOrder);
-//		} else {
-//			model.addAttribute("orderNew", orderNew);
-//		}
 		model.addAttribute("orderNew", orderNew);
-		// System.out.println(newOrder.toString());
-		// getAllProducts(model);
 		return "web/order/addOrder";
 	}
 
@@ -125,16 +79,13 @@ public class OrderController {
 		Orders newOrder = serviceOrder.addOrder(new Orders(0, new Date(), customer2.getId(), BigDecimal.valueOf(0),
 				BigDecimal.valueOf(0), BigDecimal.valueOf(0)));
 		model.addAttribute("orderNew", newOrder);
-		System.out.println(newOrder);
 		return "web/order/addOrder";
 	}
 
 	@PostMapping("/addOrderDetail")
-	public String addOrderDetails(Integer idProduct, @ModelAttribute("orderNew") Orders orderNew, Model model) {
-		System.out.println("id product: " + idProduct);
+	public String addOrderDetails(Integer idProduct, @ModelAttribute("orderNew") Orders orderNew, Model model, Integer quantity, Integer productPrice) {
 		OrdersDetails newOrderDetail = serviceOrderDetail.addOrderDetails(new OrdersDetails(0, orderNew.getId(),
-				idProduct, 0, BigDecimal.valueOf(0), 0, productOrder.getProduct(idProduct)));
-		// model.addAttribute("orderNew", idOrder);
+				idProduct, quantity, BigDecimal.valueOf(productPrice), 0, productOrder.getProduct(idProduct)));
 		return "web/order/addOrder";
 	}
 
