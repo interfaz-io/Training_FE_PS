96fdc35357508b57fb3158432161974267862af6
Jose Zamora <jzamoras@interfaz.io>
2021-08-27 16:07:12 -0600

    Validaciones Add Customer

--- src/main/java/io/interfaz/training/controllers/CustomerController.java
+++ src/main/java/io/interfaz/training/controllers/CustomerController.java
@@ -2,9 +2,15 @@
  * 
  */
 package io.interfaz.training.controllers;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,8 +44,19 @@ public class CustomerController {
 	
 	
 	@PostMapping("/add")
-	public String addCustomer(@ModelAttribute Customers customers) {
-		System.out.print(customers);
+	public String addCustomer(@ModelAttribute  @Valid Customers customers,BindingResult result, Model model) {
+		
+		if (result.hasErrors()) {
+
+			Map<String, String> errores = new HashMap<>();
+			result.getFieldErrors().forEach(err -> {
+				errores.put(err.getField(), err.getDefaultMessage());
+			});
+			model.addAttribute("error", errores);
+			return "web/customer/addCustomer";
+		}
+		
+		//System.out.print(customers);
 		serviceCustomer.addCustomer(customers);
 		return "redirect:/customer";
 	}
